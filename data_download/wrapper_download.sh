6b250d758c7d1a4627837b452e2816fba6d899f2
eli scheele <eli@drift.bio>
2021-10-19 16:27:55 -0700

    adding biopython and deps

--- data_download/wrapper_download.sh
+++ data_download/wrapper_download.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # you may need to set the installed python interpreter and the directories
 
-META_FILE=/media/patbaa/hdd1/AMR_data_avail/20190510.tsv
-PROJECT_DIR=/media/patbaa/hdd1/AMR_data_avail/
-MIC_DIR=/media/patbaa/hdd1/AMR_data_avail/MICs/
-FASTQ_DIR=/media/patbaa/hdd1/AMR_data_avail/FASTQs/
+META_FILE=/home/jovyan/meta.tsv
+PROJECT_DIR=/home/jovyan
+MIC_DIR=/home/jovyan/MICs
+FASTQ_DIR=/home/jovyan/FASTQs
 
 ./mic_download.py     --meta_file $META_FILE --mic_dir $MIC_DIR --project_dir $PROJECT_DIR
 ./fastq_download.py   --meta_file $META_FILE --fastq_dir $FASTQ_DIR
